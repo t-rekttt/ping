7225630a469f7599c2c98aa9597522aa67f29299
Việt Thảo <t.to.the.rekt@gmail.com>
2018-09-21 15:47:18 +0700

    Change url file link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const async = require('async');
 const express = require('express');
 const app = express();
 
-getUrls = () => request('https://gist.githubusercontent.com/t-rekttt/55b4e5fc6bb049344701512c6d6c9b77/raw/742843aa08afbaefe2981113d2234c5746080df1/ping_list.txt').then(resp => resp.trim().split('\n'))
+getUrls = () => request('https://gist.githubusercontent.com/t-rekttt/55b4e5fc6bb049344701512c6d6c9b77/raw/').then(resp => resp.trim().split('\n'))
 
 setInterval(() => {
   try {
