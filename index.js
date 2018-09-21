60a01f5e08bb1a69db5c3ac0167597d143717513
Việt Thảo <t.to.the.rekt@gmail.com>
2018-09-21 15:34:35 +0700

    Added router

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const request = require('request-promise');
 const async = require('async');
+const express = require('express');
+const app = express();
 
 getUrls = () => request('https://gist.githubusercontent.com/t-rekttt/55b4e5fc6bb049344701512c6d6c9b77/raw/742843aa08afbaefe2981113d2234c5746080df1/ping_list.txt').then(resp => resp.trim().split('\n'))
 
@@ -18,3 +20,12 @@ setInterval(() => {
     console.log(err);
   }
 }, 60*5*1000)
+
+app.get('', (req, res) => {
+  res.send('Hello world!');
+})
+
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+})
